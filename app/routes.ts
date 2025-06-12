e93adcfcaf9cb2769e10656996258126dab3c8c9
Charles Ponti <cj@ponti.io>
2025-06-11 21:50:21 -0700

    feat: update playground route to point to new component and add playground.tsx file

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("/playground", "routes/home.tsx"),
+  route("/playground", "routes/playground.tsx"),
   route("/border-linear-gradient", "routes/border-linear-gradient.tsx"),
 ] satisfies RouteConfig;
